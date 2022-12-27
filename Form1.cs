8ca69e2b597d6ab18093635494707c93ab452b38
zihanaulia17 <116043452+zihanaulia17@users.noreply.github.com>
2022-12-27 12:21:30 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace TrafficSignal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            RedStopLight.Visible = true;
+            YellowStopLight.Visible = false;
+            GreenStopLight.Visible = false;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (RedStopLight.Visible = true)
+            {
+                RedStopLight.Visible = false;
+                YellowStopLight.Visible = false;
+                GreenStopLight.Visible = true;
+            }
+
+            else if (GreenStopLight.Visible == true)
+            {
+                RedStopLight.Visible = false;
+                YellowStopLight.Visible = true;
+                GreenStopLight.Visible = false;
+            }
+            else if (YellowStopLight.Visible == true)
+            {
+                RedStopLight.Visible = true;
+                YellowStopLight.Visible = false;
+                GreenStopLight.Visible = false;
+            }
+        }
+        private void btnStart_Click(oject sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+        private void btnStop_Click(oject sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
